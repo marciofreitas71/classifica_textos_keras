25d93e1ebc3ee26e562d56997fdde48ec1ada2d7
marciofreitas71 <marciofreitas71@yahoo.com.br>
2024-03-20 00:38:26 -0300

    Algoritmo de treinamento e salvamento de modelos e vocabulário

--- sinapses.py
+++ sinapses.py
@@ -96,7 +96,7 @@ def train_model(texts, labels):
     model.fit(
         X_train, y_train,
         validation_data=(X_test, y_test),
-        epochs=10, batch_size=100,
+        epochs=2, batch_size=100,
         callbacks=[
             ModelCheckpoint(filepath='modelo/modelo_cnn_h5.keras', monitor='val_accuracy', save_best_only=True),
             EarlyStopping(monitor='val_accuracy', patience=4)
